d819d9c2fd5a83c0d8fff56b24806ebeead7cddb
rizkianp-laptop <140038212+rizkianp@users.noreply.github.com>
2024-01-13 21:09:18 +0700

    add scroll to top button

--- scripts/main.js
+++ scripts/main.js
@@ -5,3 +5,23 @@ function onClick(element) {
   document.getElementById("modal01").style.display = "block";
   document.getElementById("caption").innerHTML = element.alt;
 }
+
+// scroll to top button:
+let topButton = document.getElementById("btntop")
+
+// when scroll down 30px from top show the button
+window.onscroll = function() {scrollFunction()};
+
+function scrollFunction() {
+  if (document.body.scrollTop > 30 || document.documentElement.scrollTop > 30) {
+    topButton.style.display = "block";
+  } else {
+    topButton.style.display = "none";
+  }
+}
+
+//when button clicked, scroll to top
+function goToTop() {
+  document.body.scrollTop = 0; //For Safari
+  document.documentElement.scrollTop = 0; // For chrome, ie, firefox, opera
+}
