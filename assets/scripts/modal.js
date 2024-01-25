2ab72a407d5372e1b7f0ebf9d40a4a95f79d63f5
rizand <rizkianpbiz@gmail.com>
2024-01-25 08:22:42 +0700

    replace everything with newdh

---
+++ assets/scripts/modal.js
@@ -0,0 +1,35 @@
+//get the modal
+let modal = document.getElementById("modal01");
+let img = document.getElementById("img01");
+
+function onClick(element) {
+    if (element.classList.contains("done")) {
+        return;
+    } else {
+        img.src = element.src;
+        modal.style.display = "block";
+    }
+    
+}
+
+
+let span = document.getElementsByClassName("close")[0];
+
+span.onclick = function () {
+    modal.style.display = "none";
+}
+img.onclick = function () {
+    modal.style.display = "none";
+}
+
+let totalJamaah = 1200
+document.getElementById("jamaah").innerHTML = totalJamaah
+
+// click to page section
+
+function goToSection(id) {
+    let section = document.getElementById(id);
+    if (section) {
+        section.scrollIntoView({behavior: 'smooth'});
+    }
+}
