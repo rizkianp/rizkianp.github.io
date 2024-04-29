14740d3e22f1fe4c7f270233e3022d27720fadc7
rizand <rizkianpbiz@gmail.com>
2024-04-29 08:13:20 +0700

    splide js implementation

--- assets/scripts/basic.js
+++ assets/scripts/basic.js
@@ -1,10 +1,10 @@
+// SplideJS Initialization
 document.addEventListener( 'DOMContentLoaded', function() {
-    var splide = new Splide( '#splidetestimoni', {
-        perPage: 1,
-        focus  : 0,
-        omitEnd: true,
-        } );
-      splide.mount();
-
+var splide = new Splide( '#splidetestimoni', {
+  perPage: 1,
+  focus  : 0,
+  omitEnd: true,
+  } );
+splide.mount();
 
 } );
