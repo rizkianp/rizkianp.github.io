e5f3943f5044ae18ea474f5199e6fa289fac8487
rizand <rizkianpbiz@gmail.com>
2024-04-29 07:25:32 +0700

    test new

---
+++ assets/scripts/basic.js
@@ -0,0 +1,10 @@
+document.addEventListener( 'DOMContentLoaded', function() {
+    var splide = new Splide( '#splidetestimoni', {
+        perPage: 1,
+        focus  : 0,
+        omitEnd: true,
+        } );
+      splide.mount();
+
+
+} );
